e143dc41f4906b378548b715cda52f1f593965f0
AJ Eaglin <ajayeaglin@gmail.com>
2026-01-23 17:00:43 -0500

    refactor: Use ComponentProps<typeof Image> for cleaner type inference in FixedImage

--- components/FixedImage.tsx
+++ components/FixedImage.tsx
@@ -1,4 +1,8 @@
-import Image, { ImageProps as NextImageProps } from "next/image";
+import Image from "next/image";
+import type { ComponentProps } from "react";
+
+// Use ComponentProps<typeof Image> to get the props type
+type NextImageProps = ComponentProps<typeof Image>;
 
 export default function FixedImage(props: NextImageProps) {
   return <Image {...props} loading="eager" />;
