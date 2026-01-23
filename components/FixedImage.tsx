8f376d2d9cc12b793a992f48b2b0aaf2e98633fd
AJ Eaglin <ajayeaglin@gmail.com>
2026-01-23 18:43:43 -0500

    refactor: Make src required in FixedImageProps

--- components/FixedImage.tsx
+++ components/FixedImage.tsx
@@ -1,14 +1,16 @@
 // components/FixedImage.tsx
 import Image, { ImageProps } from 'next/image';
 
-export interface FixedImageProps extends ImageProps {
-  fallbackSrc?: string; // optional fallback image if src not found
+export interface FixedImageProps extends Omit<ImageProps, 'src'> {
+  src: string; // make src required
+  fallbackSrc?: string; // optional fallback image
 }
 
-export default function FixedImage({ fallbackSrc, ...props }: FixedImageProps) {
+export default function FixedImage({ src, fallbackSrc, ...props }: FixedImageProps) {
   return (
     <Image
       {...props}
+      src={src}
       onError={(e: any) => {
         if (fallbackSrc) e.currentTarget.src = fallbackSrc;
       }}
