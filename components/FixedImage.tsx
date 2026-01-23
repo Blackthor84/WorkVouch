cf2a96d40c1339ab220044a913a115e4fb4f0477
AJ Eaglin <ajayeaglin@gmail.com>
2026-01-23 17:04:58 -0500

    feat: Add unoptimized prop to FixedImage interface

--- components/FixedImage.tsx
+++ components/FixedImage.tsx
@@ -1,13 +1,14 @@
 import Image, { ImageLoaderProps } from "next/image";
 import type { HTMLAttributes } from "react";
 
-// Minimal type for FixedImage props
+// Add `unoptimized` and `className` to the type
 interface FixedImageProps extends HTMLAttributes<HTMLImageElement> {
   src: string;
   alt: string;
   width?: number;
   height?: number;
   loader?: (props: ImageLoaderProps) => string;
+  unoptimized?: boolean;
 }
 
 export default function FixedImage(props: FixedImageProps) {
